ecf9b4904f6918f29b65c09e7f1c253c0fbdbc28
Mark Eliasen <mark.eliasen@zolidsolutions.com>
2019-05-05 17:36:59 +0100

    update database tables

--- src/watchdog/database/tables.sql
+++ src/watchdog/database/tables.sql
@@ -1,16 +1,3 @@
-CREATE TABLE IF NOT EXISTS `imported_logs` (
-  `file_name` varchar(60) NOT NULL,
-  PRIMARY KEY (`file_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `players` (
-  `player_bisid` varchar(44) NOT NULL,
-  `player_steamid` varchar(17) NOT NULL,
-  `player_name` varchar(32) NOT NULL,
-  PRIMARY KEY (`player_bisid`),
-  KEY `player_steamid` (`player_steamid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS `damage` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `timestamp` varchar(19) NOT NULL,
@@ -20,9 +7,11 @@ CREATE TABLE IF NOT EXISTS `damage` (
   `attacker_bisid` varchar(44) NOT NULL,
   `attacker_pos` varchar(25) NOT NULL,
   `attacker_npc` varchar(35) NOT NULL,
+  `body_part` varchar(50) NOT NULL,
   `damage` decimal(6,2) NOT NULL,
   `weapon` varchar(35) NOT NULL,
   `distance` decimal(8,4) NOT NULL,
+  `logfile_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `player_bisid` (`player_bisid`),
   KEY `attacker_bisid` (`attacker_bisid`)
@@ -38,7 +27,31 @@ CREATE TABLE IF NOT EXISTS `killed` (
   `attacker_npc` varchar(35) NOT NULL,
   `weapon` varchar(35) NOT NULL,
   `distance` decimal(8,4) NOT NULL,
+  `logfile_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `player_bisid` (`player_bisid`),
   KEY `attacker_bisid` (`attacker_bisid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `link_tokens` (
+  `discord_id` varchar(22) NOT NULL,
+  `token` varchar(25) NOT NULL,
+  PRIMARY KEY (`discord_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `file_name` (`file_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `discord_id` varchar(22) NOT NULL,
+  `player_bisid` varchar(44) NOT NULL,
+  `player_steamid` varchar(17) NOT NULL,
+  `player_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`player_bisid`),
+  KEY `player_steamid` (`player_steamid`),
+  KEY `discord_id` (`discord_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
